18b83125cb9b87153939b67113f1bcebdefa9559
David Hernando <david.hernando@cleverea.com>
2025-10-14 15:25:50 +0200

    feat(db): create interface for abstrating database factories

--- app/shared/containers/database.py
+++ app/shared/containers/database.py
@@ -1,20 +1,17 @@
 from contextlib import asynccontextmanager
-from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker
-from sqlmodel.ext.asyncio.session import AsyncSession
+
+from app.shared.infrastructure.db.interface import AsyncDatabaseFactory
 
 
 class Database:
-    def __init__(
-        self,
-        db_url: str,
-        async_engine: AsyncEngine,
-        async_session: async_sessionmaker[AsyncSession],
-    ):
+    def __init__(self, db_url: str, database_factory: AsyncDatabaseFactory):
         self._db_url = db_url
 
         # Single engine instance reused across the app lifetime
-        self._async_engine = async_engine(url=self._db_url)
-        self._session_factory = async_session(engine=self._async_engine)
+        self._async_engine = database_factory.build_async_engine(url=self._db_url)
+        self._session_factory = database_factory.build_async_session_factory(
+            engine=self._async_engine
+        )
 
     @asynccontextmanager
     async def session_factory(self):
--- app/shared/containers/main.py
+++ app/shared/containers/main.py
@@ -3,12 +3,10 @@ from dependency_injector import containers, providers
 from app.contexts.customers.container.customer_services import CustomerServices
 from app.shared.containers.database import Database
 from app.shared.infrastructure.db.sqlite_async import (
-    build_async_engine as sqlite_build_async_engine,
-    build_async_session_factory as sqlite_build_async_session_factory,
+    SQLiteDatabaseFactory,
 )
 from app.shared.infrastructure.db.postgresql_async import (
-    build_async_engine as postgres_build_async_engine,
-    build_async_session_factory as postgres_build_async_session_factory,
+    PostgresDatabaseFactory,
 )
 
 
@@ -16,18 +14,13 @@ class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
 
     sqlite_database = providers.Singleton(
-        Database,
-        db_url=config.sqlite_url,
-        async_engine=sqlite_build_async_engine,
-        async_session=sqlite_build_async_session_factory,
+        Database, db_url=config.sqlite_url, database_factory=SQLiteDatabaseFactory
     )
 
     postgres_database = providers.Singleton(
-        Database,
-        db_url=config.postgres_url,
-        async_engine=postgres_build_async_engine,
-        async_session=postgres_build_async_session_factory,
+        Database, db_url=config.postgres_url, database_factory=PostgresDatabaseFactory
     )
+
     database = postgres_database
     customer_services = providers.Container(
         CustomerServices, session_factory=database.provided.session_factory

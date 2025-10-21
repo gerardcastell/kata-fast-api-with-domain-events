441336e9950b35c13f4d64b66e0d4a0d5775ecd7
Gerard Castell <gerardcastell97@gmail.com>
2025-10-21 16:14:02 +0200

    feat(typechecking): add typecheck with mypy + fix type errors

--- app/contexts/customers/application/customer_searcher.py
+++ app/contexts/customers/application/customer_searcher.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 from app.contexts.customers.domain.entities.customer import Customer
 from app.contexts.customers.domain.repositories.customer_repository import (
     CustomerRepository,
@@ -8,8 +10,8 @@ class CustomerSearcher:
     def __init__(self, customer_repository: CustomerRepository):
         self.customer_repository = customer_repository
 
-    async def search_by_id(self, id: str) -> Customer:
+    async def search_by_id(self, id: str) -> Customer |None:
         return await self.customer_repository.find_by_id(id)
 
-    async def search_all(self) -> list[Customer]:
+    async def search_all(self) -> Iterable[Customer]:
         return await self.customer_repository.find_all()
--- app/contexts/customers/container/customer_services.py
+++ app/contexts/customers/container/customer_services.py
@@ -1,17 +1,18 @@
 from dependency_injector import containers, providers
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.contexts.customers.application.customer_creator import CustomerCreator
 from app.contexts.customers.application.customer_searcher import CustomerSearcher
 from app.contexts.customers.infrastructure.persistence.sqlite_customer_repository import (
-    SQLiteCustomerRepository,
+    PostgreSQLCustomerRepository,
 )
 
 
 class CustomerServices(containers.DeclarativeContainer):
-    session_factory = providers.Dependency()
+    session_factory = providers.Dependency[AsyncSession]()
 
     customer_repository = providers.Factory(
-        SQLiteCustomerRepository, session_factory=session_factory
+        PostgreSQLCustomerRepository, session_factory=session_factory
     )
 
     customer_creator = providers.Factory(CustomerCreator, customer_repository=customer_repository)
